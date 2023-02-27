ebd4ef5950a0fc7d588046db955e4603725b7af1
aroma-lim <hyangki.lim@gmail.com>
2023-02-28 00:55:07 +0900

    feat: redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import "./App.css";
 import TodoInput from "./components/TodoInput";
 import TodoItem from "./components/TodoItem";
+import { useAppDispatch, useAppSelector } from "./hook/hooks";
+import { selectTodos, setTodos } from "./store/todosSlice";
 import { TODO } from "./type/types";
 
 function App() {
+  const dispatch = useAppDispatch();
+
   const data = localStorage.getItem("todoList") ?? "[]";
-  const json = JSON.parse(data);
+  const json: TODO[] = JSON.parse(data);
+  dispatch(setTodos(json));
+
+  const todos = useAppSelector(selectTodos);
 
   return (
     <div className="container">
       <h1>Marq-TODO</h1>
       <TodoInput />
-      {json.map((todo: TODO) => (
-        <TodoItem todo={todo} />
-      ))}
+      {todos.length
+        ? todos.map((todo: TODO) => <TodoItem key={todo.id} todo={todo} />)
+        : json.map((todo: TODO) => <TodoItem key={todo.id} todo={todo} />)}
     </div>
   );
 }
---
+++ src/store/todosSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "./store";
+import { TODO } from "../type/types";
+
+interface TodosState {
+  todos: TODO[];
+}
+
+const initialState: TodosState = {
+  todos: [],
+};
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    setTodo: (state, action: PayloadAction<TODO>) => {
+      state.todos = [...state.todos, action.payload];
+    },
+    setTodos: (state, action: PayloadAction<TODO[]>) => {
+      state.todos = action.payload;
+    },
+  },
+});
+
+export const { setTodo, setTodos } = todosSlice.actions;
+
+export const selectTodos = (state: RootState) => state.todos.todos;
+
+export default todosSlice.reducer;
