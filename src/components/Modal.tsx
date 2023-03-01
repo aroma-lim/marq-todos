9e259b12bf5cb14f8dd76d1204f4fa4e61a09028
aroma-lim <hyangki.lim@gmail.com>
2023-03-02 05:46:59 +0900

    feat: refer in modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useRef, useState } from "react";
 import { TODO } from "../type/types";
 import { SlClose, SlArrowDown, SlArrowUp } from "react-icons/sl";
+import { CgClose } from "react-icons/cg";
 import { callApiWithData } from "../util/api";
 import { useAppDispatch, useAppSelector } from "../hook/hooks";
 import { selectTodos, updateTodo } from "../store/todosSlice";
@@ -18,6 +19,7 @@ const Modal: FC<Props> = (props: Props) => {
   const todos = useAppSelector(selectTodos);
 
   const [title, setTitle] = useState<string>(todo.title);
+  const [refer, setRefer] = useState<TODO[]>(todo.refer);
   const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
   const [dropdownMenu, setDropdownMenu] = useState<TODO[]>();
 
@@ -33,7 +35,7 @@ const Modal: FC<Props> = (props: Props) => {
     const newTodo: TODO = {
       id: todo.id,
       title: title,
-      refer: todo.refer,
+      refer: refer,
       done: todo.done,
       createdDate: todo.createdDate,
       editedDate: new Date().toLocaleDateString(),
@@ -55,10 +57,43 @@ const Modal: FC<Props> = (props: Props) => {
     }
   };
 
+  const handleSelect = (target: TODO) => {
+    setRefer([...refer, target]);
+    setDropdownMenu((prev) => {
+      const newMenu = prev?.filter((t: TODO) => t.id !== target.id);
+      return newMenu;
+    });
+    setDropdownOpen(false);
+  };
+
+  const handleDeselect = (target: TODO) => {
+    setDropdownMenu((prev) => {
+      const newMenu = prev
+        ? todos
+            .filter(
+              (t: TODO) =>
+                prev.findIndex((p: TODO) => p.id === t.id) >= 0 ||
+                t.id === target.id
+            )
+            .filter((t: TODO) => t.id !== todo.id)
+        : todos.filter((t: TODO) => t.id !== todo.id);
+      return newMenu;
+    });
+    setRefer((prev) => {
+      const newMenu = prev?.filter((t: TODO) => t.id !== target.id);
+      return newMenu;
+    });
+  };
+
   useEffect(() => {
-    const newMenu = todos.filter((t: TODO) => t.id !== todo.id);
+    const newMenu = todos
+      .filter(
+        (t: TODO) =>
+          todo.refer.findIndex((refer: TODO) => refer.id === t.id) < 0
+      )
+      .filter((t: TODO) => t.id !== todo.id);
     setDropdownMenu(newMenu);
-  }, [todos, todo.id]);
+  }, [todos, todo.id, todo.refer]);
 
   return (
     <div
@@ -83,7 +118,9 @@ const Modal: FC<Props> = (props: Props) => {
           ></input>
 
           <button
-            className={`dropdown-button ${dropdownMenu ? "" : "disabled"}`}
+            className={`dropdown-button ${
+              dropdownMenu?.length ? "" : "disabled"
+            }`}
             disabled={!dropdownMenu}
             onClick={() => setDropdownOpen(!dropdownOpen)}
           >
@@ -95,9 +132,22 @@ const Modal: FC<Props> = (props: Props) => {
                 <div
                   key={menu.id}
                   className="dropdown-menu"
-                  onClick={() => setDropdownOpen(false)}
+                  onClick={() => handleSelect(menu)}
                 >
-                  @ {menu.title}
+                  {menu.title}
+                </div>
+              ))}
+            </div>
+          )}
+          {refer && (
+            <div className="refer-container">
+              {refer.map((r: TODO) => (
+                <div className="refer">
+                  {r.title} &nbsp;{" "}
+                  <CgClose
+                    style={{ width: "10px", cursor: "pointer" }}
+                    onClick={() => handleDeselect(r)}
+                  />
                 </div>
               ))}
             </div>
@@ -105,9 +155,11 @@ const Modal: FC<Props> = (props: Props) => {
         </div>
 
         <button
-          className={`modal-button ${title === todo.title ? "disabled" : ""}`}
+          className={`modal-button ${
+            title === todo.title && refer === todo.refer ? "disabled" : ""
+          }`}
           onClick={handleEdit}
-          disabled={title === todo.title}
+          disabled={title === todo.title && refer === todo.refer}
         >
           수정하기
         </button>
